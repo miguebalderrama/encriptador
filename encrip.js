d3b8686500709b4bfd59a743a172c578ccaaf4ed
miguebalderrama <42938621+miguebalderrama@users.noreply.github.com>
2022-05-14 00:54:06 -0300

    ya esta casi listo

--- encrip.js
+++ encrip.js
@@ -8,37 +8,82 @@
  const messageDesencriptar = document.querySelector("#messageDesencriptar");
  const textoValue = document.querySelector("#texto");
 
-const limpiar = () => {
-    let elemento = document.getElementById("messageEncriptado");
-    textoValue.value = "";
-    messageDesencriptar.style.display = "block";
-    if(elemento){
-        document.getElementById("resultadoTexto").removeChild(elemento);
+
+    const limpiar = () => {
+        let elemento = document.getElementById("messageEncriptado");
+        textoValue.value = "";
+        messageDesencriptar.style.display = "block";
+        if(elemento){
+            document.getElementById("resultadoTexto").removeChild(elemento);
+        }
+        document.getElementById("btnCopiar").style.display = "none";
+
     }
-}
+    
+    const encriptar = () => {
+        messageDesencriptar.style.display = "none"; 
+        let elemento = document.getElementById("messageEncriptado");
+        if(elemento){
+            document.getElementById("resultadoTexto").removeChild(elemento);
+        }   
+        let elementEncriptado = document.createElement("p");
+        elementEncriptado.id = "messageEncriptado"; 
+        elementEncriptado.className = "messageEncriptado";   
+        let texto = textoValue.value;
+        texto = texto.toLowerCase();        
+        let textoEncriptado = texto.replace("e", "enter");        
+        textoEncriptado = textoEncriptado.replaceAll("i", "imes");
+        textoEncriptado = textoEncriptado.replaceAll("a", "ai");
+        textoEncriptado = textoEncriptado.replaceAll("o", "ober");
+        textoEncriptado = textoEncriptado.replaceAll("u", "ufat");
+        elementEncriptado.innerHTML = textoEncriptado;
+        document.getElementById("resultadoTexto").appendChild(elementEncriptado);
+        const list = document.getElementById("resultadoTexto");
+        list.insertBefore(elementEncriptado, list.children[0]);        
+        document.getElementById("btnCopiar").style.display = "block";
+    }
+    
+    const desencriptar = () => {
+        messageDesencriptar.style.display = "none";
+        let elemento = document.getElementById("messageEncriptado");
+        if(elemento){
+            document.getElementById("resultadoTexto").removeChild(elemento);
+        }
+        let elementEncriptado = document.createElement("p");
+        elementEncriptado.id = "messageEncriptado";
+        elementEncriptado.className = "messageEncriptado";   
+        let texto = textoValue.value;
+        texto = texto.toLowerCase();        
+        let textoEncriptado = texto.replaceAll("enter", "e");        
+        textoEncriptado = textoEncriptado.replaceAll("imes", "i");
+        textoEncriptado = textoEncriptado.replaceAll("ai", "a");
+        textoEncriptado = textoEncriptado.replaceAll("ober", "o");
+        textoEncriptado = textoEncriptado.replaceAll("ufat", "u");
+        elementEncriptado.innerHTML = textoEncriptado;
+        document.getElementById("resultadoTexto").appendChild(elementEncriptado);
+        const list = document.getElementById("resultadoTexto");
+        list.insertBefore(elementEncriptado, list.children[0]);        
+        document.getElementById("btnCopiar").style.display = "block";
 
-const encriptar = () => {
-    messageDesencriptar.style.display = "none";
-    let elementEncriptado = document.createElement("p");
-    elementEncriptado.id = "messageEncriptado";    
-    let texto = textoValue.value;
-    texto = texto.toLowerCase();
-    console.log(texto);
-    let textoEncriptado = texto.replace("e", "enter");
-    console.log(textoEncriptado);
-    textoEncriptado = textoEncriptado.replaceAll("i", "imes");
-    textoEncriptado = textoEncriptado.replaceAll("a", "ai");
-    textoEncriptado = textoEncriptado.replaceAll("o", "ober");
-    textoEncriptado = textoEncriptado.replaceAll("u", "ufat");
-    elementEncriptado.innerHTML = textoEncriptado;
-    document.getElementById("resultadoTexto").appendChild(elementEncriptado);
-}
+    }
 
+    const copiar = () => {
+        let elemento = document.getElementById("messageEncriptado");        
+        navigator.clipboard.writeText(elemento.innerHTML);
+        console.log(elemento.innerHTML);
+          }
+   
 
+    const btnEncriptar = document.getElementById("btnEncriptar");
+    btnEncriptar.addEventListener("click", encriptar);
 
-
- const btnEncriptar = document.getElementById("btnEncriptar");
- btnEncriptar.addEventListener("click", encriptar);
-
- const btnClean = document.getElementById("btnClean");
+    const btnClean = document.getElementById("btnClean");
     btnClean.addEventListener("click", limpiar);
+
+    const btnDesencriptar = document.getElementById("btnDesencriptar");
+    btnDesencriptar.addEventListener("click", desencriptar);
+
+    const btnCopiar = document.getElementById("btnCopiar");
+    btnCopiar.addEventListener("click", copiar);
+    
+    
